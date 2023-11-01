82622f8113f0f51be4c869e63fce6063428646dd
Amadej Glasenčnik <amadej.glasencnik@gmail.com>
2023-11-01 14:42:17 +0100

    Add Snippets.pkgdef file and the placeholder snippets, Update vsixmanifest, Add constants, Add source generators nuget package

---
+++ BlazoryVS/BlazoryVSDefaults.cs
@@ -0,0 +1,46 @@
+﻿namespace BlazoryVS
+{
+    /// <summary>
+    /// Represents the default constant values.
+    /// </summary>
+    internal class BlazoryVSDefaults
+    {
+        #region Path constants
+
+        /// <summary>
+        /// Gets the name of the folder that contains the snippets.
+        /// </summary>
+        public const string SnippetsFolderName = "Snippets";
+
+        /// <summary>
+        /// Gets the name of the folder that contains the C# snippets.
+        /// </summary>
+        public const string CSharpSnippetsFolderName = "Blazory CSharp Snippets";
+
+        /// <summary>
+        /// Gets the name of the folder that contains the Razor snippets.
+        /// </summary>
+        public const string RazorSnippetsFolderName = "Blazory Razor Snippets";
+
+        #endregion
+
+        #region Snippet constants
+
+        /// <summary>
+        /// Gets the name of the snippet author.
+        /// </summary>
+        public const string SnippetAuthor = "Blazory";
+
+        /// <summary>
+        /// Gets the name of the C# snippet language.
+        /// </summary>
+        public const string CSharpSnippetLanguage = "CSharp";
+
+        /// <summary>
+        /// Gets the name of the Razor snippet language.
+        /// </summary>
+        public const string RazorSnippetLanguage = "Razor";
+
+        #endregion
+    }
+}
--- BlazoryVS/BlazoryVSPackage.cs
+++ BlazoryVS/BlazoryVSPackage.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.VisualStudio.Shell;
+using Microsoft.VisualStudio.Shell.Interop;
 using System;
 using System.Runtime.InteropServices;
 using System.Threading;
@@ -7,28 +8,16 @@ using Task = System.Threading.Tasks.Task;
 namespace BlazoryVS
 {
     /// <summary>
-    /// This is the class that implements the package exposed by this assembly.
+    /// Represents the entry extension class.
     /// </summary>
-    /// <remarks>
-    /// <para>
-    /// The minimum requirement for a class to be considered a valid package for Visual Studio
-    /// is to implement the IVsPackage interface and register itself with the shell.
-    /// This package uses the helper classes defined inside the Managed Package Framework (MPF)
-    /// to do it: it derives from the Package class that provides the implementation of the
-    /// IVsPackage interface and uses the registration attributes defined in the framework to
-    /// register itself and its components with the shell. These attributes tell the pkgdef creation
-    /// utility what data to put into .pkgdef file.
-    /// </para>
-    /// <para>
-    /// To get loaded into VS, the package must be referred by &lt;Asset Type="Microsoft.VisualStudio.VsPackage" ...&gt; in .vsixmanifest file.
-    /// </para>
-    /// </remarks>
     [PackageRegistration(UseManagedResourcesOnly = true, AllowsBackgroundLoading = true)]
     [Guid(BlazoryVSPackage.PackageGuidString)]
+    [ProvideAutoLoad(UIContextGuids.NoSolution, PackageAutoLoadFlags.BackgroundLoad)]
+    [ProvideAutoLoad(UIContextGuids.SolutionExists, PackageAutoLoadFlags.BackgroundLoad)]
     public sealed class BlazoryVSPackage : AsyncPackage
     {
         /// <summary>
-        /// BlazoryVSPackage GUID string.
+        /// Gets the BlazoryVSPackage GUID string.
         /// </summary>
         public const string PackageGuidString = "a0775924-0734-4872-b19d-3a1a56325a58";
 
@@ -43,9 +32,7 @@ namespace BlazoryVS
         /// <returns>A task representing the async work of package initialization, or an already completed task if there is none. Do not return null from this method.</returns>
         protected override async Task InitializeAsync(CancellationToken cancellationToken, IProgress<ServiceProgressData> progress)
         {
-            // When initialized asynchronously, the current thread may be a background thread at this point.
-            // Do any initialization that requires the UI thread after switching to the UI thread.
-            await this.JoinableTaskFactory.SwitchToMainThreadAsync(cancellationToken);
+            await JoinableTaskFactory.SwitchToMainThreadAsync(cancellationToken);
         }
 
         #endregion
